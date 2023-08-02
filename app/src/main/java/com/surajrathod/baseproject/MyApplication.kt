623e86fae60e51248688e2a1f23762ce7e79558a
SurajSinh Rathod <surajsinhrathod75@gmail.com>
2023-08-02 18:43:17 +0530

    Hilt setup done

---
+++ app/src/main/java/com/surajrathod/baseproject/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.surajrathod.baseproject
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+
+
+}
